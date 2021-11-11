3ec6af45e8d6feb2e03b53cbd7d10156de168de6
Yohanes <ysirorus11@gmail.com>
2021-11-11 08:27:17 +0700

    update

--- application/controllers/Myticket.php
+++ application/controllers/Myticket.php
@@ -1,76 +1,77 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Myticket extends CI_Controller {
+class Myticket extends CI_Controller
+{
 
-function __construct(){
-        parent::__construct();
-        $this->load->model('model_app');
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('model_app');
 
-        if(!$this->session->userdata('id_user'))
-       {
-        $this->session->set_flashdata("msg", "<div class='alert alert-info'>
+		if (!$this->session->userdata('id_user')) {
+			$this->session->set_flashdata("msg", "<div class='alert alert-info'>
        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
        <strong><span class='glyphicon glyphicon-remove-sign'></span></strong> Silahkan login terlebih dahulu.
        </div>");
-        redirect('login');
-        }
-    }
+			redirect('login');
+		}
+	}
 
 
- function myticket_list()
- {
+	function myticket_list()
+	{
 
- 	    $data['header'] = "header/header";
-        $data['navbar'] = "navbar/navbar";
-        $data['sidebar'] = "sidebar/sidebar";
+		$data['header'] = "header/header";
+		$data['navbar'] = "navbar/navbar";
+		$data['sidebar'] = "sidebar/sidebar";
 		$data['active12'] = "active";
-        $data['body'] = "body/myticket";
+		$data['body'] = "body/myticket";
 
-        $id_dept = trim($this->session->userdata('id_dept'));
-        $id_user = trim($this->session->userdata('id_user'));
+		$id_dept = trim($this->session->userdata('id_dept'));
+		$id_user = trim($this->session->userdata('id_user'));
 
-        //notification 
+		//notification 
 
-        $sql_listticket = "SELECT COUNT(id_ticket) AS jml_list_ticket FROM ticket WHERE status = 2";
-        $row_listticket = $this->db->query($sql_listticket)->row();
+		$sql_listticket = "SELECT COUNT(id_ticket) AS jml_list_ticket FROM ticket WHERE status = 2";
+		$row_listticket = $this->db->query($sql_listticket)->row();
 
-        $data['notif_list_ticket'] = $row_listticket->jml_list_ticket;
+		$data['notif_list_ticket'] = $row_listticket->jml_list_ticket;
 
-        $sql_approvalticket = "SELECT COUNT(A.id_ticket) AS jml_approval_ticket FROM ticket A 
+		$sql_approvalticket = "SELECT COUNT(A.id_ticket) AS jml_approval_ticket FROM ticket A 
         LEFT JOIN sub_kategori B ON B.id_sub_kategori = A.id_sub_kategori 
         LEFT JOIN kategori C ON C.id_kategori = B.id_kategori
         LEFT JOIN karyawan D ON D.nik = A.reported 
         LEFT JOIN bagian_departemen E ON E.id_bagian_dept = D.id_bagian_dept WHERE E.id_dept = $id_dept AND status = 1";
-        $row_approvalticket = $this->db->query($sql_approvalticket)->row();
+		$row_approvalticket = $this->db->query($sql_approvalticket)->row();
 
-        $data['notif_approval'] = $row_approvalticket->jml_approval_ticket;
+		$data['notif_approval'] = $row_approvalticket->jml_approval_ticket;
 
-        $sql_assignmentticket = "SELECT COUNT(id_ticket) AS jml_assignment_ticket FROM ticket WHERE status = 3 AND id_teknisi='$id_user'";
-        $row_assignmentticket = $this->db->query($sql_assignmentticket)->row();
+		$sql_assignmentticket = "SELECT COUNT(id_ticket) AS jml_assignment_ticket FROM ticket WHERE status = 3 AND id_teknisi='$id_user'";
+		$row_assignmentticket = $this->db->query($sql_assignmentticket)->row();
 
-        $data['notif_assignment'] = $row_assignmentticket->jml_assignment_ticket;
+		$data['notif_assignment'] = $row_assignmentticket->jml_assignment_ticket;
 
-        //end notification
+		//end notification
 
-        $id = trim($this->session->userdata('id_user'));
+		$id = trim($this->session->userdata('id_user'));
 
-        $datamyticket = $this->model_app->datamyticket($id);
-	    $data['datamyticket'] = $datamyticket;
-        
-        $this->load->view('template', $data);
- }
+		$datamyticket = $this->model_app->datamyticket($id);
+		$data['datamyticket'] = $datamyticket;
+
+		$this->load->view('template', $data);
+	}
 
 
- function myticket_detail($id)
- {
-    $data['header'] = "header/header";
-        $data['navbar'] = "navbar/navbar";
-        $data['sidebar'] = "sidebar/sidebar";
+	function myticket_detail($id)
+	{
+		$data['header'] = "header/header";
+		$data['navbar'] = "navbar/navbar";
+		$data['sidebar'] = "sidebar/sidebar";
 		$data['active12'] = "active";
-        $data['body'] = "body/progress_teknisi";
+		$data['body'] = "body/progress_teknisi";
 
-         $sql = "SELECT A.status, A.progress, A.tanggal, A.tanggal_solved, A.tanggal_proses, A.tanggal_solved, F.nama AS nama_teknisi, D.nama, C.id_kategori, A.id_ticket, A.problem_summary, A.problem_detail, A.tanggal, A.file, B.nama_sub_kategori, C.nama_kategori, G.nama_bagian_dept, H.nama_dept, I.nama_kondisi
+		$sql = "SELECT A.status, A.progress, A.tanggal, A.tanggal_solved, A.tanggal_proses, A.tanggal_solved, F.nama AS nama_teknisi, D.nama, C.id_kategori, A.id_ticket, A.problem_summary, A.problem_detail, A.tanggal, A.file, B.nama_sub_kategori, C.nama_kategori, G.nama_bagian_dept, H.nama_dept, I.nama_kondisi, I.id_kondisi, A.status, A.durasi_solved
                 FROM ticket A 
                 LEFT JOIN sub_kategori B ON B.id_sub_kategori = A.id_sub_kategori
                 LEFT JOIN kategori C ON C.id_kategori = B.id_kategori 
@@ -82,145 +83,143 @@ function __construct(){
 				LEFT JOIN kondisi I ON I.id_kondisi = A.id_kondisi
                 WHERE A.id_ticket = '$id'";
 
-        $row = $this->db->query($sql)->row();
+		$row = $this->db->query($sql)->row();
 
-        $id_kategori = $row->id_kategori;
+		$id_kategori = $row->id_kategori;
 
-        $data['dd_teknisi'] = $this->model_app->dropdown_teknisi($id_kategori);
-        $data['id_teknisi'] = "";
-            
-        $data['id_ticket'] = $id;  
-        $data['nama_teknisi'] = $row->nama_teknisi;       
+		$data['dd_teknisi'] = $this->model_app->dropdown_teknisi($id_kategori);
+		$data['id_teknisi'] = "";
+
+		$data['id_ticket'] = $id;
+		$data['nama_teknisi'] = $row->nama_teknisi;
 		$data['nama_dept'] = $row->nama_dept;
 		$data['nama_divisi'] = $row->nama_bagian_dept;
-        $data['tanggal'] = $row->tanggal;
-        $data['nama_sub_kategori'] = $row->nama_sub_kategori;
-        $data['nama_kategori'] = $row->nama_kategori;
-        $data['problem_summary'] = $row->problem_summary;
-        $data['problem_detail'] = $row->problem_detail;
+		$data['tanggal'] = $row->tanggal;
+		$data['nama_sub_kategori'] = $row->nama_sub_kategori;
+		$data['nama_kategori'] = $row->nama_kategori;
+		$data['problem_summary'] = $row->problem_summary;
+		$data['problem_detail'] = $row->problem_detail;
 		$file = explode("/", $row->file);
 		$data['file_name'] = $file[count($file) - 1];
-        $data['file'] = $row->file;
-        $data['reported'] = $row->nama;
-        $data['progress'] = $row->progress;
-        $data['tanggal_proses'] = $row->tanggal_proses;
+		$data['file'] = $row->file;
+		$data['reported'] = $row->nama;
+		$data['progress'] = $row->progress;
+		$data['tanggal_proses'] = $row->tanggal_proses;
 
-        $data['tanggal'] = $row->tanggal;
-        $data['tanggal_solved'] = $row->tanggal_solved;
-        $data['priority'] = $row->nama_kondisi;
-
-        //TRACKING TICKET
-        $data_trackingticket = $this->model_app->data_trackingticket($id);
-        $data['data_trackingticket'] = $data_trackingticket;
-
-        
-        $this->load->view('template', $data);
- }
-
-
- function feedback_yes($id_ticket, $id_teknisi)
- {
-
-    
-
-    $id_user = trim($this->session->userdata('id_user'));
-    $tanggal = $time = date("Y-m-d  H:i:s");
-
-    $data['feedback'] = 1;
-    $data['reported'] = $id_user;
-    $data['id_ticket'] = $id_ticket;
+		$data['tanggal'] = $row->tanggal;
+		$data['tanggal_solved'] = $row->tanggal_solved;
+		$data['priority'] = $row->nama_kondisi;
+		$data['id_priority'] = $row->id_kondisi;
+		$data['status_ticket'] = $row->status;
+		$data['durasi_solved'] = $row->durasi_solved;
+		
+		//TRACKING TICKET
+		$data_trackingticket = $this->model_app->data_trackingticket($id);
+		$data['data_trackingticket'] = $data_trackingticket;
+		
+		$datalibur = [];
+		$queryLibur = $this->db->query("SELECT tanggal FROM libur")->result();
+		foreach ($queryLibur as $libur) {
+			array_push($datalibur, $libur->tanggal);
+		}
+		$data['datasla'] = $this->model_app->datasla();
+		$data['datalibur'] = $datalibur;
+		$this->load->view('template', $data);
+	}
 
 
-    $sql_teknisi = "SELECT * FROM teknisi WHERE id_teknisi = '$id_teknisi'";
-    $row = $this->db->query($sql_teknisi)->row();
-
-    $data2['point'] = $row->point + 1;
-  
-    $this->db->trans_start();
-
-    $this->db->insert('history_feedback', $data);
-
-    $this->db->where('id_teknisi', $id_teknisi);
-    $this->db->update('teknisi', $data2);
-
-    $this->db->trans_complete();
-
-    if ($this->db->trans_status() === FALSE)
-            {
-               
-                redirect('myticket/myticket_list');   
-            } else 
-            {
-                
-                redirect('myticket/myticket_list');   
-            }
+	function feedback_yes($id_ticket, $id_teknisi)
+	{
 
 
 
- }
+		$id_user = trim($this->session->userdata('id_user'));
+		$tanggal = $time = date("Y-m-d  H:i:s");
 
- function feedback_no($id_ticket, $id_teknisi)
- {
+		$data['feedback'] = 1;
+		$data['reported'] = $id_user;
+		$data['id_ticket'] = $id_ticket;
 
-    
 
-    $id_user = trim($this->session->userdata('id_user'));
-    $tanggal = $time = date("Y-m-d  H:i:s");
+		$sql_teknisi = "SELECT * FROM teknisi WHERE id_teknisi = '$id_teknisi'";
+		$row = $this->db->query($sql_teknisi)->row();
 
-    $data['feedback'] = 0;
-    $data['reported'] = $id_user;
-    $data['id_ticket'] = $id_ticket;
+		$data2['point'] = $row->point + 1;
 
-    $sql_teknisi = "SELECT * FROM teknisi WHERE id_teknisi = '$id_teknisi'";
-    $row = $this->db->query($sql_teknisi)->row();
+		$this->db->trans_start();
 
-    $data2['point'] = $row->point - 1;
-  
-    $this->db->trans_start();
+		$this->db->insert('history_feedback', $data);
 
-    $this->db->insert('history_feedback', $data);
+		$this->db->where('id_teknisi', $id_teknisi);
+		$this->db->update('teknisi', $data2);
 
-    $this->db->where('id_teknisi', $id_teknisi);
-    $this->db->update('teknisi', $data2);
+		$this->db->trans_complete();
 
-    $this->db->trans_complete();
+		if ($this->db->trans_status() === FALSE) {
 
-    if ($this->db->trans_status() === FALSE)
-            {
-               
-                redirect('myticket/myticket_list');   
-            } else 
-            {
-                
-                redirect('myticket/myticket_list');   
-            }
- }
-    
-public function pdfmyticket()
-    {
-    
-    $id = trim($this->session->userdata('id_user'));
+			redirect('myticket/myticket_list');
+		} else {
 
-        $datamyticket = $this->model_app->datamyticket($id);
-        $data['datamyticket'] = $datamyticket;
-    
-    
-    ob_start();
-        $content = $this->load->view('body/pdfmyticket',$data);
-        $content = ob_get_clean();      
-        $this->load->library('html2pdf');
-        try
-        {
-            $html2pdf = new HTML2PDF('L', 'A4', 'en');
-            $html2pdf->pdf->SetDisplayMode('fullpage');
-            $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
-            $html2pdf->Output('Report_ppic.pdf');
-        }
-        catch(HTML2PDF_exception $e) {
-            echo $e;
-            exit;
-        }
-    
-    }
+			redirect('myticket/myticket_list');
+		}
+	}
 
+	function feedback_no($id_ticket, $id_teknisi)
+	{
+
+
+
+		$id_user = trim($this->session->userdata('id_user'));
+		$tanggal = $time = date("Y-m-d  H:i:s");
+
+		$data['feedback'] = 0;
+		$data['reported'] = $id_user;
+		$data['id_ticket'] = $id_ticket;
+
+		$sql_teknisi = "SELECT * FROM teknisi WHERE id_teknisi = '$id_teknisi'";
+		$row = $this->db->query($sql_teknisi)->row();
+
+		$data2['point'] = $row->point - 1;
+
+		$this->db->trans_start();
+
+		$this->db->insert('history_feedback', $data);
+
+		$this->db->where('id_teknisi', $id_teknisi);
+		$this->db->update('teknisi', $data2);
+
+		$this->db->trans_complete();
+
+		if ($this->db->trans_status() === FALSE) {
+
+			redirect('myticket/myticket_list');
+		} else {
+
+			redirect('myticket/myticket_list');
+		}
+	}
+
+	public function pdfmyticket()
+	{
+
+		$id = trim($this->session->userdata('id_user'));
+
+		$datamyticket = $this->model_app->datamyticket($id);
+		$data['datamyticket'] = $datamyticket;
+
+
+		ob_start();
+		$content = $this->load->view('body/pdfmyticket', $data);
+		$content = ob_get_clean();
+		$this->load->library('html2pdf');
+		try {
+			$html2pdf = new HTML2PDF('L', 'A4', 'en');
+			$html2pdf->pdf->SetDisplayMode('fullpage');
+			$html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+			$html2pdf->Output('Report_ppic.pdf');
+		} catch (HTML2PDF_exception $e) {
+			echo $e;
+			exit;
+		}
+	}
 }
